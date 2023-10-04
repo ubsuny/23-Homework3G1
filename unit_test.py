8e60c0e983b53f438e8bb9fe35dcc873ea536f5e
uarif <uarif@users.noreply.github.com>
2023-10-03 22:13:34 -0400

    Update unit_test.py
    Made some changes

--- unit_test.py
+++ unit_test.py
@@ -1,40 +1,37 @@
 import unittest
 import math
 
-# Define the function to calculate the periodic time
 def calculate_periodic_time(l):
-    """Calculates the periodic time of a simple pendulum.
+  """Calculates the periodic time of a simple pendulum.
 
-    Args:
-        l: The length of the pendulum in meters.
+  Args:
+    l: The length of the pendulum in meters.
 
-    Returns:
-        The periodic time of the pendulum in seconds.
-    """
-    # Acceleration due to gravity in meters per second squared.
-    g = 9.81
-    
-    # Calculate the periodic time using the formula
-    periodic_time = 2 * math.pi * math.sqrt(l / g)
-    
-    return periodic_time
+  Returns:
+    The periodic time of the pendulum in seconds.
+  """
+  # Acceleration due to gravity in meters per second squared.
+  g = 9.81
+
+  # Use lambda function calculate the periodic time where l is the length of the string and return the value calculated
+  calculate_periodic_time = (lambda l: 2 * math.pi / math.sqrt(g / l))
+  return calculate_periodic_time
 
 class TestCalculatePeriodicTime(unittest.TestCase):
 
-    def test_calculate_periodic_time(self):
-        # Test cases with known results
-        test_cases = [
-            (0.5, 2.007),  # (length, expected_periodic_time)
-            (1.0, 4.498),
-            (1.5, 6.283),
-            (2.0, 8.987),
-            (2.5, 11.178)
-        ]
+  def test_calculate_periodic_time(self):
+    # Define a list of lengths of pendulums
+    x = [0.5, 1, 1.5, 2, 2.5]
 
-        # Perform the tests
-        for length, expected_periodic_time in test_cases:
-            result = calculate_periodic_time(length)
-            assert math.isclose(result, expected_periodic_time, rel_tol=0.001)
+    # Calculate the expected periodic times
+    expected_periodic_times = [1.57, 3.14, 4.44, 5.66, 6.84]
+
+    # Use the calculate_periodic_time() function to calculate the periodic times
+    actual_periodic_times = map(calculate_periodic_time, x)
+
+    # Assert that the actual periodic times are equal to the expected periodic times
+    for i in range(len(x)):
+      assert actual_periodic_times[i] == expected_periodic_times[i]
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
